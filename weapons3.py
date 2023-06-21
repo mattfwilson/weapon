b890109163df9ce4620d9d092481d320f6246f1a
mattfwilson <mattfwilson@gmail.com>
2023-06-21 16:32:15 -0400

    Main roll conditional created

--- weapons3.py
+++ weapons3.py
@@ -1,3 +1,12 @@
+CHAR_CLASS = ''
 RARITY = {'Legendary': 2.0, 'Rare': 17.5, 'Uncommon': 30.5, 'Common': 50.0}
 GEN_ATTR = {'Strength', False, 'Intelligence', True, 'Dexerity', False, 'Willpower', False, 'Vitality', False}
-GEN_ATTR_RNG = [6.0, 16.0]
+GEN_ATTR_RNG = {'is_class': [6.0, 16.0], 'not_class': [4.0, 12.0]}
+
+CHAR_CLASS = input('What is your class? ')
+roll = ''
+
+if roll == 'y':
+    pass
+else:
+    roll = input('Roll weapon? (y/n) ')
