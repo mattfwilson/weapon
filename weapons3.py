1913289db8965595bf961ca56f10f4de31c5a43b
mattfwilson <mattfwilson@gmail.com>
2023-06-21 16:02:48 -0400

    New beginning of D4 weapons rolling

---
+++ weapons3.py
@@ -0,0 +1,3 @@
+RARITY = {'Legendary': 2.0, 'Rare': 17.5, 'Uncommon': 30.5, 'Common': 50.0}
+GEN_ATTR = {'Strength', False, 'Intelligence', True, 'Dexerity', False, 'Willpower', False, 'Vitality', False}
+GEN_ATTR_RNG = [6.0, 16.0]
