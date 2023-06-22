4dfbb30493650caf73ba0148beaa5cf221651763
mattfwilson <mattfwilson@gmail.com>
2023-06-22 13:06:35 -0400

    Renamed main func

--- weapons3.py
+++ weapons3.py
@@ -1,21 +1,32 @@
 CHAR_CLASS = ''
+CLASSES = ['Barbarian', 'Sorcerer', 'Rogue', 'Necromancer', 'Druid']
 RARITY = {'Legendary': 2.0, 'Rare': 17.5, 'Uncommon': 30.5, 'Common': 50.0}
 GEN_ATTR = {'Strength', False, 'Intelligence', True, 'Dexerity', False, 'Willpower', False, 'Vitality', False}
 GEN_ATTR_RNG = {'is_class': [6.0, 16.0], 'not_class': [4.0, 12.0]}
 
-CHAR_CLASS = input('What is your class? ')
-ROLL = ''
+def check_class(CHAR_CLASS, CLASSES):
+    CHAR_CLASS = input('What is your class? ')
 
-def main():
-    while True:
-        if ROLL == 'y':
-            rarity = random.choice(RARITY)
-            attribute = random.choice(GEN_ATTR)
-            print(f'Rarity: {rarity}, Attribute: {attribute}')
-        elif ROLL == 'n':
-            quit()
-        else:
-            roll = input('Roll weapon? (y/n) ')
+    if CHAR_CLASS in CLASSES:
+        return CHAR_CLASS
+    else:
+        print('Invalid class')
+
+def roll_weapon(RARITY, GEN_ATTR):
+    roll = ''
+    if roll == 'y':
+        rarity = random.choice(RARITY)
+        attribute = random.choice(GEN_ATTR)
+        print(f'Rarity: {rarity}, Attribute: {attribute}')
+    elif roll == 'n':
+        quit()
+    else:
+        roll = input('Roll weapon? (y/n) ')
 
 if __name__ == "__main__":
-    main()
+    while True:
+        CHAR_CLASS = input('What is your class? ')
+        if CHAR_CLASS in CLASSES:
+            rolled = roll_weapon(RARITY, GEN_ATTR)
+        else:
+            pass
