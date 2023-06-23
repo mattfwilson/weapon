aaf4d0b49306977f1a665291d55797c854b618fb
mattfwilson <mattfwilson@gmail.com>
2023-06-23 00:31:44 -0400

    Ensuring I can access lists as dict values

--- weapons3.py
+++ weapons3.py
@@ -5,6 +5,7 @@ RARITY = {'Legendary': 2.0, 'Rare': 17.5, 'Uncommon': 30.5, 'Common': 50.0}
 GEN_ATTR = {'Strength', False, 'Intelligence', True, 'Dexerity', False, 'Willpower', False, 'Vitality', False}
 GEN_ATTR_RNG = {'is_class': [6.0, 16.0], 'not_class': [4.0, 12.0]}
 
+print(GEN_ATTR_RNG.get('is_class')[0])
 def roll_weapon(RARITY, GEN_ATTR):
     roll = ''
     if roll == 'y':
@@ -25,4 +26,5 @@ if __name__ == "__main__":
             rolled = roll_weapon(RARITY, GEN_ATTR)
             print(f'{rolled}: (rolled func executed)')
         else:
+            print(f'Breaking loop')
             break
