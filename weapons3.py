705e01ccdcec14a7e5c35f704ae2102263d9aa16
mattfwilson <mattfwilson@gmail.com>
2023-06-23 11:56:58 -0400

    Created initial class for weapon creation

--- weapons3.py
+++ weapons3.py
@@ -1,11 +1,27 @@
+import itertools
+
 CHAR_CLASS = ''
 CHAR_LEVEL = 1
 CLASSES = ['Barbarian', 'Sorcerer', 'Rogue', 'Necromancer', 'Druid']
 RARITY = {'Legendary': 2.0, 'Rare': 17.5, 'Uncommon': 30.5, 'Common': 50.0}
 GEN_ATTR = {'Strength', False, 'Intelligence', True, 'Dexerity', False, 'Willpower', False, 'Vitality', False}
-GEN_ATTR_RNG = {'is_class': [6.0, 16.0], 'not_class': [4.0, 12.0]}
 
-print(GEN_ATTR_RNG.get('is_class')[0])
+class CommonWeapon:
+    id = itertools.count(0)
+    gen_attr_range = {
+        'is_class': [6.0, 16.0],
+        'not_class': [4.0, 12.0]
+        }
+
+    def __init__(self):
+        self.id = next(CommonWeapon.id)
+
+    def __repr__(self):
+        return f'Weapon {self.id} created!'
+
+w1 = CommonWeapon()
+print(w1)
+
 def roll_weapon(RARITY, GEN_ATTR):
     roll = ''
     if roll == 'y':
