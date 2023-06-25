897629052f110255b29a771816ca5d28caedecb3
mattfwilson2 <mattfwilson+1@gmail.com>
2023-06-25 14:53:28 -0400

    Class attribute rolling based on weapon rarity

--- weapons3.py
+++ weapons3.py
@@ -5,14 +5,14 @@ CHAR_CLASS = str()
 CHAR_LEVEL = 1
 CLASSES = ['Barbarian', 'Sorcerer', 'Rogue', 'Necromancer', 'Druid']
 RARITY = {'Legendary': 2.0, 'Rare': 17.5, 'Uncommon': 30.5, 'Common': 50.0}
-GEN_ATTR = {'Strength', False, 'Intelligence', True, 'Dexerity', False, 'Willpower', False, 'Vitality', False}
+SORCERER_ATTR = {'Strength', RARITY.get('Legendary'), 'Intelligence', RARITY.get('Common'), 'Dexerity', RARITY.get('Rare'), 'Willpower', RARITY.get('Common'), 'Vitality', RARITY.get('Uncommon')}
 
 class CommonWeapon:
-    id = itertools.count(0)
+    id_iter = itertools.count(0)
     gen_attr_range = {'is_class': [6.0, 16.0], 'not_class': [4.0, 12.0]}
 
     def __init__(self):
-        self.id = next(CommonWeapon.id)
+        self.id = next(CommonWeapon.id_iter)
 
     def __repr__(self):
         return f'Weapon {self.id} created!'
@@ -23,8 +23,13 @@ if __name__ == "__main__":
     while True:
         CHAR_CLASS = input('What is your class? ')
         if CHAR_CLASS in CLASSES:
-            rolled = roll_weapon(RARITY)
-            print(f'{rolled}: (rolled func executed)')
+            print(f'{CHAR_CLASS} is valid! Rolling weapons...')
+            w1 = CommonWeapon()
+            print(w1)
+            w2 = CommonWeapon()
+            print(w2)
+            w3 = CommonWeapon()
+            print(w3)
         else:
-            print(f'Breaking loop')
+            print(f'{CHAR_CLASS} not valid. Breaking loop')
             break
