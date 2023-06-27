47aafef01ecffc78378756ecb160eb2b151381f8
mattfwilson2 <mattfwilson@gmail.com>
2023-06-26 21:15:07 -0400

    Randomzing adjectives and weapons types inside of class

--- weapons3.py
+++ weapons3.py
@@ -5,8 +5,14 @@ CHAR_CLASS = str()
 CHAR_LEVEL = 1
 INVENTORY = []
 CLASSES = ['Barbarian', 'Sorcerer', 'Rogue', 'Necromancer', 'Druid']
-RARITY = {'Legendary': 2.0, 'Rare': 17.5, 'Uncommon': 30.5, 'Common': 50.0}
-SORCERER_ATTR = {'Strength', RARITY.get('Legendary'), 'Intelligence', RARITY.get('Common'), 'Dexerity', RARITY.get('Rare'), 'Willpower', RARITY.get('Common'), 'Vitality', RARITY.get('Uncommon')}
+RARITY = {'Legendary': 3, 'Rare': 17, 'Uncommon': 30, 'Common': 50}
+SOR_ATTR = {'Strength', RARITY.get('Legendary'), 'Intelligence', RARITY.get('Common'), 'Dexerity', RARITY.get('Rare'), 'Willpower', RARITY.get('Common'), 'Vitality', RARITY.get('Uncommon')}
+
+SOR_WEAP = ['Wand', 'Staff']
+SOR_COMMON_ADJ = ['Basic', 'Apprentice', 'Feeble']
+SOR_UNCOMMON_ADJ = ['Enchanted', 'Enlightened', 'Fanatic\'s']
+SOR_RARE_ADJ = ['Cleric\'s', 'Undying', 'Apostle\'s']
+SOR_LEGENDARY_ADJ = ['Sacred', 'Awkward', 'Elementalist\'s']
 
 class Weapon:
     id_iter = itertools.count(1)
@@ -14,9 +20,12 @@ class Weapon:
         self.id = next(Weapon.id_iter)
 
 class Common(Weapon):
-    def __init__(self):
+    def __init__(self, weapon, common_adj):
         super().__init__()
         self.item_level = self.id + 1
+        self.weapon = random.choices(weapon, cum_weights=(0.50, 0.50))
+        self.adj = random.choices(common_adj, cum_weights=(0.50, 0.35, 0.15))
+        print(f'{str(self.adj)} {str(self.weapon)}')
         INVENTORY.append(self)
 
     def __repr__(self):
@@ -29,7 +38,7 @@ if __name__ == "__main__":
     CHAR_CLASS = 'Sorcerer'
     if CHAR_CLASS in CLASSES:
         print(f'You are a {CHAR_CLASS}! Rolling weapon...')
-        common1 = Common()
+        common1 = Common(SOR_WEAP, SOR_COMMON_ADJ)
         print(f'Inventory: {INVENTORY}')
         print(f'{common1}, Item Level: {common1.get_item_level()}')
     else:
