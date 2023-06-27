6a7156d78e81c15f430963820151eb0c3a78ea95
mattfwilson2 <mattfwilson@gmail.com>
2023-06-27 00:22:09 -0400

    Concatenated adj and weapon with item level

--- weapons3.py
+++ weapons3.py
@@ -8,8 +8,8 @@ classes = ['Barbarian', 'Sorcerer', 'Rogue', 'Necromancer', 'Druid']
 rarity = {'Legendary': 3, 'Rare': 17, 'Uncommon': 30, 'Common': 50}
 sor_attr = {'Strength', rarity.get('Legendary'), 'Intelligence', rarity.get('Common'), 'Dexerity', rarity.get('Rare'), 'Willpower', rarity.get('Common'), 'Vitality', rarity.get('Uncommon')}
 
-sor_weap = ['Wand', 'Staff']
-sor_common_adj = ['Basic', 'Apprentice', 'Feeble']
+sor_weap_type = ['Wand', 'Staff', 'Dagger']
+sor_common_adj = ['Basic', 'Apprentice', 'Feeble', 'Sturdy', 'Oak']
 sor_uncommon_adj = ['Enchanted', 'Enlightened', 'Fanatic\'s']
 sor_rare_adj = ['Cleric\'s', 'Undying', 'Apostle\'s']
 sor_legendary_adj = ['Sacred', 'Awkward', 'Elementalist\'s']
@@ -22,24 +22,24 @@ class Weapon:
 class Common(Weapon):
     def __init__(self, weapon, common_adj):
         super().__init__()
-        self.item_level = self.id + 1
-        self.weapon = random.choices(weapon, cum_weights=(0.50, 0.50))
-        self.adj = random.choices(common_adj, cum_weights=(0.50, 0.35, 0.15))
-        print(f'{str(self.adj)} {str(self.weapon)}')
-        inventory.append(self)
+        self._item_level = self.id + 1
+        self._weapon = ', '.join(random.choices(weapon))
+        self._adj = ', '.join(random.choices(common_adj))
+        self._drop = self._adj + ' ' + self._weapon
+        inventory.append(self._drop)
 
     def __repr__(self):
-        return f'Weapon {self.id}'
+        return f'{str(self._adj)} {str(self._weapon)}'
 
     def get_item_level(self):
-        return self.item_level
-
+        return self._item_level
+    
 if __name__ == "__main__":
     char_class = 'Sorcerer'
     if char_class in classes:
         print(f'You are a {char_class}! Rolling weapon...')
-        common1 = Common(sor_weap, sor_common_adj)
+        common1 = Common(sor_weap_type, sor_common_adj)
         print(f'Inventory: {inventory}')
-        print(f'{common1}, Item Level: {common1.get_item_level()}')
+        print(f'{common1}, Lvl: {common1.get_item_level()}')
     else:
         print(f'{char_class} is not a valid class. Exiting...')
