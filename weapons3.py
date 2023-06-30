6dc02f87763687b64e3e13f51381185356a68c64
mattfwilson2 <mattfwilson@gmail.com>
2023-06-30 01:27:51 -0400

    Added Unique rarity

--- weapons3.py
+++ weapons3.py
@@ -6,7 +6,7 @@ CHAR_CLASS = str()
 CHAR_LEVEL = 1
 INVENTORY = []
 CLASSES = ['Barbarian', 'Sorcerer', 'Rogue', 'Necromancer', 'Druid']
-RARITY = ['Common', 'Uncommon', 'Rare', 'Legendary']
+RARITY = ['Common', 'Uncommon', 'Rare', 'Legendary', 'Unique']
 
 class Item:
     id_iter = itertools.count(1)
@@ -14,9 +14,10 @@ class Item:
         self.id = next(Item.id_iter)
 
 class Weapon(Item):
-    def __init__(self, weapon, adj):
+    def __init__(self, weapon, adj, level):
         super().__init__()
-        self._weap_lvl = self.id + 1
+        self._level = level
+        self._weap_lvl = random.choice(self._level, self._level + 1)
         self._weap_type = ', '.join(random.choices(weapon))
         self._weap_adj = ', '.join(random.choices(adj))
         self._drop = self._weap_adj + ' ' + self._weap_type
@@ -30,13 +31,13 @@ class Weapon(Item):
 
     def get_item_level(self):
         return self._weap_lvl
-    
+
 if __name__ == "__main__":
     while True:
         roll_input = input('Roll item? ')
         if roll_input in 'y':
-            rarity_roll = random.choices(RARITY, weights=[10, 5, 3, 1])
-            item = Weapon(sor_weap_type, sor_common_adj)
+            rarity_roll = random.choices(RARITY, weights=[10, 7, 4, 2, 1])
+            item = Weapon(sor_weap_type, sor_common_adj, CHAR_LEVEL)
             print(', '.join(rarity_roll))
         elif roll_input == 'inv':
             print(f'Inventory: {INVENTORY}')
@@ -45,3 +46,5 @@ if __name__ == "__main__":
                 print(item)
             print(f'Exiting...')
             break
+
+
