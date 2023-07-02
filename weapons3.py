94a0cc55f0268262e1a251d761dc6ebf67d0fb19
mattfwilson <mattfwilson@gmail.com>
2023-07-02 14:30:33 -0400

    Properly rolling and outputing adjectives and weapon types

--- weapons3.py
+++ weapons3.py
@@ -6,10 +6,7 @@ CHAR_CLASS = str()
 CHAR_LEVEL = 1
 INVENTORY = []
 CLASSES = ['Barbarian', 'Sorcerer', 'Rogue', 'Necromancer', 'Druid']
-RARITY = ['Common', 'Uncommon', 'Rare', 'Legendary', 'Unique']
-
-rarity_roll = random.choices(RARITY, weights=[10, 7, 4, 2, 1])[0]
-print(rarity_roll)
+RARITY = ['Common', 'Magic', 'Rare', 'Legendary', 'Unique']
 
 class Item:
     id_iter = itertools.count(1)
@@ -17,15 +14,15 @@ class Item:
         self.id = next(Item.id_iter)
 
 class Weapon(Item):
-    def __init__(self, rarity, adj, weapon, level):
+    def __init__(self, rarity: str, adj: dict, weapon: list):
         super().__init__()
-        self._level = level
-        self._weap_lvl = self._level + 1
-        print(random.choices(adj.get(rarity)))
-        self._weap_adj = ', '.join(random.choices(adj.get(rarity)[0]))
-        self._weap_adj = ', '.join(random.choices(adj))
-        self._weap_type = ', '.join(random.choices(weapon))
+        self._rarity = rarity
+        print(self._rarity)
+        self._level = 5
+        self._weap_adj = ''.join(random.choices(adj.get(self._rarity)))
+        self._weap_type = ''.join(random.choices(weapon))
         self._drop = self._weap_adj + ' ' + self._weap_type
+        print(self._drop)
         INVENTORY.append(self._drop)
 
     def __repr__(self):
@@ -34,21 +31,15 @@ class Weapon(Item):
     def get_id(self):
         return self.id
 
-    def get_item_level(self):
-        return self._weap_lvl
-
 if __name__ == "__main__":
     while True:
         roll_input = input('Roll item? ')
         if roll_input in 'y':
-            rarity_roll = random.choices(RARITY, weights=[10, 7, 4, 2, 1])
-            item = Weapon(rarity_roll, sor_adj, sor_weap, CHAR_LEVEL)
-            print(', '.join(rarity_roll))
+            rarity_roll = ''.join(random.choices(RARITY, weights=[10, 7, 4, 2, 1]))
+            item = Weapon(rarity_roll, sor_adj, sor_weap)
         elif roll_input == 'inv':
             print(f'Inventory: {INVENTORY}')
         else:
-            for item in INVENTORY:
-                print(item)
             print(f'Exiting...')
             break
 
