a8926dc18b62e6a365f5f11ef1c22cd451988f3f
mattfwilson <mattfwilson@gmail.com>
2023-07-03 00:20:53 -0400

    Moving methods that need to execute before init above init

--- class_info.py
+++ class_info.py
@@ -6,3 +6,11 @@ sor_adj = {
         'Unique': ['Sacred', 'Intellect\'s', 'Cultist\'s']
         }
 sor_weap = ['Wand', 'Staff', 'Dagger', 'Focus']
+sor_attrs = ['Intelligence', 'Willpower', 'Dexterity', 'Vitality', 'Strength']
+public_attrs = {
+        'Common': ['Poison Resistance', 'Cold Resistance', 'Fire Resistance', 'Lightning Resistance', 'Shadow Resistance', 'Undead Resistance'],
+        'Uncommon': ['Armor', 'Dodge Chance', 'Healing from Potions'],
+        'Rare': ['Life', 'Life on Kill', 'Overpower Chance', 'Critical Hit Chance', 'Critical Hit Damage', 'Damage vs Controlled', 'Damage'],
+        'Legendary': ['Damage with Skills', 'Damage with Basic', 'Damage with Core', 'Damage vs Elites', 'Damage with Skills'],
+        'Unique': ['Movement Speed', 'Weapon Damage', 'Weapon Speed', 'Vulnerable Damage', 'Resist All', 'Attack Speed']
+        }
--- weapons3.py
+++ weapons3.py
@@ -13,25 +13,9 @@ class Item:
         self.id = next(Item.id_iter)
         self.type_equipment = True
         self.type_consumable = False
-        self.type_quest = False
+        self.account_bound = False
 
 class Weapon(Item):
-    def __init__(self, rarity: str, adj: dict, weapon: list):
-        super().__init__()
-        self._rarity = rarity
-        self._level = 5
-        self._weap_adj = ''.join(random.choices(adj.get(self._rarity)))
-        self._weap_type = ''.join(random.choices(weapon))
-        self._drop = self._weap_adj + ' ' + self._weap_type
-        self._buff_slots = 0
-        INVENTORY.append(self._drop)
-
-    def __repr__(self):
-        return self._adj, self._weap_type
-
-    def get_id(self):
-        return self.id
-
     def check_rarity(self):
         if self._rarity == 'Unique':
             self._rarity = self._rarity.upper()
@@ -45,22 +29,51 @@ class Weapon(Item):
             self._buff_slots = 2
         else:
             self._buff_slots = 1
+        roll_attrs(self._buff_slots)
+
+    def roll_attrs(self, slots):
+        self._buffs.append(random.choices(sor_attrs.get(self._rarity, weights=[10, 7, 3, 2, 1])))
+        slots -= 1
+        if slots > 0:
+            for slot in range(slots):
+                self._buffs.append(random.choices(public_attrs.get(self._rarity, weights[5, 4, 3, 2, 1])))
+        return self._buffs
+
+    def __init__(self, rarity: str, adj: dict, weapon: list):
+        super().__init__()
+        self._rarity = rarity
+        self._level = 5
+        self._weap_adj = ''.join(random.choices(adj.get(self._rarity)))
+        self._weap_type = ''.join(random.choices(weapon))
+        self._drop = self._weap_adj + ' ' + self._weap_type
+        self._buff_slots = check_rarity()
+        self._buffs = []
+        roll_attrs()
+        INVENTORY.append(self._drop)
+
+    def __repr__(self):
+        return self._adj, self._weap_type
+
+    def get_id(self):
+        return self.id
 
     def show_drop(self):
         print(f'[{self._rarity}] {self._drop}')
 
     def show_details(self):
-        print(f'Equipment: {self.type_equipment}\nConsumable: {self.type_consumable}\nQuest item: {self.type_quest}\nBuff slots: {self._buff_slots}')
+        print(f'Equipment: {self.type_equipment}\nConsumable: {self.type_consumable}\nQuest item: {self.account_bound}\nBuff slots: {self._buff_slots}')
+
+def show_full_drop(item):
+    item.show_drop()
+    item.show_details()
 
 if __name__ == "__main__":
     while True:
-        roll_input = input('Roll item? ')
-        if roll_input in 'y':
+        roll_input = input('What do you want to do? ')
+        if roll_input in 'roll':
             rarity_roll = ''.join(random.choices(RARITY, weights=[10, 7, 4, 2, 1]))
             item = Weapon(rarity_roll, sor_adj, sor_weap)
-            item.check_rarity()
-            item.show_drop()
-            item.show_details()
+            show_full_drop(item)
         elif roll_input == 'inv':
             print(f'Inventory: {INVENTORY}')
         else:
